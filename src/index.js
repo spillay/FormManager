4a56873dc188fbc107b78937580911af77967846
BhaskarVeligeti <bhaskarv20@gmail.com>
2018-05-08 01:07:52 +0200

    React Dates

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import DynamicForm from './DynamicForm.jsx';
 import BlankForm from './BlankForm.jsx'
-import {ReactDatesDefaultThemeOverRide} from './ReactDatesDefaultThemeOverRide'
+// import {ReactDatesDefaultThemeOverRide} from './ReactDatesDefaultThemeOverRide'
 //import { builtinModules } from 'module';
 import 'bootstrap'; // This line will only import the js part of bootstrap. 
 import 'bootstrap-select' // This line will only import the js part of bootstrap-select.
 import $ from 'jquery';
 import './Styles/styles.scss' // scss files
+import 'react-dates/lib/css/_datepicker.css'
+import 'react-dates/initialize' 
 
-
-export {DynamicForm,BlankForm,ReactDatesDefaultThemeOverRide}
+export {DynamicForm,BlankForm}
