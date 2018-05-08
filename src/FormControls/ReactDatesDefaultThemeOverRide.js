ac003e40fecf29ba1da5ce06815e00a8de2656ed
BhaskarVeligeti <bhaskarv20@gmail.com>
2018-05-08 15:07:29 +0200

    react-dates update

--- src/FormControls/ReactDatesDefaultThemeOverRide.js
+++ src/FormControls/ReactDatesDefaultThemeOverRide.js
@@ -8,27 +8,27 @@ ThemedStyleSheet.registerInterface(aphroditeInterface);
 const ReactDatesDefaultThemeOverRide = ThemedStyleSheet.registerTheme({
   reactDates: {
     ...DefaultTheme.reactDates,
-    color: {
-      ...DefaultTheme.reactDates.color,
-      // backgroundFocused: '#0187b4',
-      placeholderText: '#0187b4',
-    },
+    // color: {
+    //   ...DefaultTheme.reactDates.color,
+    //   // backgroundFocused: '#0187b4',
+    //   placeholderText: '#0187b4',
+    // },
     sizing: {
       inputWidth: 105,
-      // arrowWidth: 20,
+      arrowWidth: 20,
     },
 
-    // font: {
-    //   size: 12,
-    //   captionSize: 12,
-    //   input: {
-    //     size: 13,
-    //     lineHeight: '13px',
-    //     size_small: 13,
-    //     lineHeight_small: '13px',
+    font: {
+      size: 12,
+      captionSize: 12,
+      input: {
+        size: 13,
+        lineHeight: '13px',
+        size_small: 13,
+        lineHeight_small: '13px',
 
-    //   },
-    // },
+      },
+    },
 
   },
 });
