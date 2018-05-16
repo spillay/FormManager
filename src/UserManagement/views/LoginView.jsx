93f8eed01b40cfa6d41b196ec3a50d6743e9d36f
BhaskarVeligeti <bhaskarv20@gmail.com>
2018-05-16 13:33:12 +0200

    ucm styles

--- src/UserManagement/views/LoginView.jsx
+++ src/UserManagement/views/LoginView.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import AuthService from '../service/AuthService';
 import ErrorBoundary from '../../utils/ErrorBoundary';
 import { DynamicForm } from '../../DynamicForm';
-import '../styles/styles.scss'
+import '../styles/loginview.css'
 
 // import 'bootstrap';
 // import 'bootstrap-select';
