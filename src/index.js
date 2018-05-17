9ab34edcddb5d1e38d8814d5fda263f63b34e649
BhaskarVeligeti <bhaskarv20@gmail.com>
2018-05-17 21:45:43 +0200

    reset

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import {DynamicForm} from './DynamicForm.jsx';
-import {BlankForm} from './BlankForm.jsx'
-import {LoginView} from './UserManagement/views/LoginView'
-import {RegisterView} from './UserManagement/views/RegisterView'
+import { DynamicForm } from './DynamicForm.jsx';
+import { BlankForm } from './BlankForm.jsx'
+import { LoginView } from './UserManagement/views/LoginView'
+import { RegisterView } from './UserManagement/views/RegisterView'
+import { ResetView } from './UserManagement/views/ResetView'
 
 
-
-export {DynamicForm,BlankForm,LoginView,RegisterView}
+export { DynamicForm, BlankForm, LoginView, RegisterView, ResetView }
